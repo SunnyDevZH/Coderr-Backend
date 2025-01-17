08c289dd2749f86423fb52193221f0720fe64714
SunnyDevZH <yannick.vaterlaus@windowslive.com>
2025-01-17 17:33:05 +0100

    admin

--- mein_backend/accounts/admin.py
+++ mein_backend/accounts/admin.py
@@ -4,16 +4,16 @@ from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('email', 'username', 'is_staff', 'is_active',)
-    list_filter = ('email', 'username', 'is_staff', 'is_active',)
+    list_display = ('email', 'username', 'user_type', 'is_staff', 'is_active',)
+    list_filter = ('email', 'username', 'user_type', 'is_staff', 'is_active',)
     fieldsets = (
-        (None, {'fields': ('email', 'password', 'username')}),
+        (None, {'fields': ('email', 'password', 'username', 'user_type')}),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'username', 'password1', 'password2', 'is_staff', 'is_active')}
+            'fields': ('email', 'username', 'password1', 'password2', 'user_type', 'is_staff', 'is_active')}
         ),
     )
     search_fields = ('email', 'username',)
